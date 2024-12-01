4faa86f521634a41b8b8c08b7b31e022df78a5a1
Michael W. Aziz <michael@micazi.dev>
2024-12-01 13:27:14 -0500

    [DEV]

--- lib/src/console/helper/methods/choose_option.dart
+++ lib/src/console/helper/methods/choose_option.dart
@@ -1,84 +1,3 @@
-// import 'package:dart_console/dart_console.dart';
-// import '../../../../console_helper.dart';
-
-// List<String> chooseImpl(
-//   String promptMessage,
-//   List<String> options, {
-//   bool allowMultiple = false,
-// }) {
-//   // Console-helping
-//   final console = Console();
-//   // Hide the cursor before user input
-//   console.hideCursor();
-
-//   // Clear screen and display the message
-//   console.clearScreen();
-//   console.writeLine(promptMessage);
-
-//   int currentSelection = 0;
-//   final selectedIndices = <int>{}; // To track selected options in multi-select
-
-//   void renderOptions() {
-//     for (int i = 0; i < options.length; i++) {
-//       console.cursorPosition = Coordinate(i + 3, 0); // Adjust position for options
-
-//       final isSelected = selectedIndices.contains(i);
-//       final isHighlighted = i == currentSelection;
-
-//       final indicator = isHighlighted ? '→' : ' '; // Arrow for current option
-//       final checkbox = isSelected ? '[x]' : '[ ]'; // Checkbox for selection
-
-//       console.writeLine('$indicator $checkbox ${options[i]}');
-//     }
-//   }
-
-//   void updateOption(int index, bool highlight, bool selected) {
-//     console.cursorPosition = Coordinate(index + 3, 0); // Adjust position for options
-
-//     final indicator = highlight ? '→' : ' ';
-//     final checkbox = selected ? '[x]' : '[ ]';
-
-//     console.writeLine('$indicator $checkbox ${options[index]}');
-//   }
-
-//   // Initial render
-//   renderOptions();
-
-//   while (true) {
-//     final key = console.readKey();
-
-//     if (key.controlChar == ControlCharacter.arrowUp) {
-//       final previousSelection = currentSelection;
-//       currentSelection = (currentSelection - 1) % options.length;
-//       if (currentSelection < 0) currentSelection += options.length; // Wrap around
-//       updateOption(previousSelection, false, selectedIndices.contains(previousSelection));
-//       updateOption(currentSelection, true, selectedIndices.contains(currentSelection));
-//     } else if (key.controlChar == ControlCharacter.arrowDown) {
-//       final previousSelection = currentSelection;
-//       currentSelection = (currentSelection + 1) % options.length;
-//       updateOption(previousSelection, false, selectedIndices.contains(previousSelection));
-//       updateOption(currentSelection, true, selectedIndices.contains(currentSelection));
-//     } else if (allowMultiple && key.char == ' ') {
-//       if (selectedIndices.contains(currentSelection)) {
-//         selectedIndices.remove(currentSelection);
-//       } else {
-//         selectedIndices.add(currentSelection);
-//       }
-//       updateOption(currentSelection, true, selectedIndices.contains(currentSelection));
-//     } else if (key.controlChar == ControlCharacter.enter) {
-//       ConsoleHelper.writeSpace();
-//       // Show cursor after selection is complete
-//       console.showCursor();
-
-//       if (allowMultiple) {
-//         return selectedIndices.map((index) => options[index]).toList();
-//       } else {
-//         return [options[currentSelection]];
-//       }
-//     }
-//   }
-// }
-
 import 'dart:io';
 
 bool _isMultiSelect = false;
@@ -156,6 +75,11 @@ List<int> chooseImpl(String title, List<String> options, {bool multiSelect = fal
         break;
       case 13: // Enter (carriage return)
       case 10: // Enter (newline)
+        if (!_isMultiSelect && selected.every((s) => !s)) {
+          // Automatically select the current index if nothing was toggled
+          selected.fillRange(0, options.length, false);
+          selected[currentIndex] = true;
+        }
         cleanup();
         stdout.writeln();
         return List<int>.generate(options.length, (i) => selected[i] ? i : -1).where((index) => index != -1).toList();
